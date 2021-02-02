8475d003fa6a55f8ac20e87c173512247f93d4ac
MalikWhitfield <malik.whitfield@associa.us>
2021-02-02 16:03:34 -0500

    try to add swagger

--- VenueEmailing/Startup.cs
+++ VenueEmailing/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.Swagger;
 
 namespace VenueEmailing
 {
@@ -26,6 +27,10 @@ namespace VenueEmailing
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new Info { title = "Values Api", version = "v1" });
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
